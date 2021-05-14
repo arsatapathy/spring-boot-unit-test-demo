eedbbf31fde963f839577c7c9e15010e04ec6322
arsatapathy <ashish.satapathy2014@gmail.com>
2021-05-14 18:37:15 +0530

    Baseline

---
+++ src/test/java/com/arsatapathy/spring/boot/testing/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsatapathy.spring.boot.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
